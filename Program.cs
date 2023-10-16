ebb57161e1f0a41841b2e3f6e3ad6e728c056ab6
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-16 12:19:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_number_contains_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 87501;
+            int digitToCheck = 3;
+
+            bool containsDigit = number.ToString().Contains(digitToCheck.ToString());
+
+            if (containsDigit)
+                Console.WriteLine(number + "Contains the digit " + digitToCheck);
+
+            else
+                Console.WriteLine(number + " does not contain" + digitToCheck);
+            Console.WriteLine();
+        }
+    }
+}
